79763bff0e525e1c4ad818f0858ec3ca217cc07a
doankhiem <khiembi161219@gmail.com>
2024-02-23 09:09:53 +0700

    props component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 // import Food from "./Food"
 import Card from "./components/Card"
 import Button from "./components/Button"
+import Student from "./components/Student"
 function App() {
   return (
     <>
@@ -12,6 +13,13 @@ function App() {
       <Card />
       <Card />
       <Button />
+      <div>
+        <Student name="Huy" age={20} isStudent={false} />
+        <Student name="Quân" age={45} isStudent={true} />
+        <Student name="Chiến" age={55} isStudent={false} />
+        <Student name="Hòa" age={33} isStudent={true} />
+        <Student />
+      </div>
     </>
     
   )
