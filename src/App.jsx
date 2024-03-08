3b7d83ef04b9dd77fe6c5c9d99f7e44c961e444b
khiemdev <khiembi161219@gmail.com>
2024-03-08 10:01:21 +0700

    color picker app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import List from "./components/List"
 import MyComponent from "./components/MyComponent"
 import Counter from "./components/Counter"
 import Form from "./components/Form"
+import ColorPicker from "./components/ColorPicker"
 function App() {
   const fruits = [
         { id: 1, name: 'grape', calogies: 52 }, 
@@ -48,6 +49,7 @@ function App() {
       <MyComponent />
       <Counter />
       <Form />
+      <ColorPicker />
     </>
     
   )
